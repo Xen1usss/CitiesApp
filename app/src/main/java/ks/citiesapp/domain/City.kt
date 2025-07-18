c96da2473fddc874bc09b8a37d50de3d818248c5
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-07-19 00:51:00 +0300

    make draft of CitiesScreen.kt

---
+++ app/src/main/java/ks/citiesapp/domain/City.kt
@@ -0,0 +1,6 @@
+package ks.citiesapp.domain
+
+data class City(
+    val name: String,
+    val yearFounded: String
+)
--- app/src/main/java/ks/citiesapp/ui/screen/cities/CitiesScreen.kt
+++ app/src/main/java/ks/citiesapp/ui/screen/cities/CitiesScreen.kt
@@ -1,9 +1,50 @@
 package ks.citiesapp.ui.screen.cities
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import ks.citiesapp.domain.City
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showBackground = true)
 @Composable
 fun CitiesScreen() {
-    Text(text = "Города экран")
+    val cities = listOf(
+        City("Париж", "III век до н.э."),
+        City("Вена", "1147 год"),
+        City("Берлин", "1237 год"),
+        City("Варшава", "1321 год"),
+        City("Милан", "1899 год")
+    )
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(cities, key = { it.name }) { city ->
+            CityItem(city = city)
+        }
+    }
+
+
+}
+
+@Composable
+fun CityItem(city: City) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = city.name)
+        Text(text = city.yearFounded)
+    }
 }
