8071cd647980bd4d6a77f293bb10d17d49218835
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-07-22 23:10:21 +0300

    add text hardcode

--- app/src/main/java/ks/citiesapp/ui/screen/cities/CitiesScreen.kt
+++ app/src/main/java/ks/citiesapp/ui/screen/cities/CitiesScreen.kt
@@ -1,16 +1,30 @@
 package ks.citiesapp.ui.screen.cities
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ks.citiesapp.domain.City
-import org.burnoutcrew.reorderable.*
+import org.burnoutcrew.reorderable.ReorderableItem
+import org.burnoutcrew.reorderable.detectReorderAfterLongPress
+import org.burnoutcrew.reorderable.rememberReorderableLazyListState
+import org.burnoutcrew.reorderable.reorderable
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -34,6 +48,15 @@ fun CitiesScreen(
             .reorderable(state)
             .detectReorderAfterLongPress(state)
     ) {
+        item {
+            Text(
+                text = "Города",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+            )
+        }
         items(citiesState, key = { it.name }) { city ->
             ReorderableItem(state, key = city.name) { isDragging ->
                 CityItem(
